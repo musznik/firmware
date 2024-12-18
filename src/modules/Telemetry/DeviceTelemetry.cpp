7a719d9ea810178b276975b7943db5c143c1f46a
musznik <bymucha@gmail.com>
2024-12-18 15:49:58 +0100

    fixing phone packet detection

--- src/modules/Telemetry/DeviceTelemetry.cpp
+++ src/modules/Telemetry/DeviceTelemetry.cpp
@@ -202,7 +202,7 @@ bool DeviceTelemetryModule::sendTelemetry(NodeNum dest, bool phoneOnly)
     nodeDB->updateTelemetry(nodeDB->getNodeNum(), telemetry, RX_SRC_LOCAL);
     if (phoneOnly) {
         LOG_INFO("Send packet to phone");
-        p->priority = static_cast<meshtastic_MeshPacket_Priority>(-1);
+        p->priority = static_cast<meshtastic_MeshPacket_Priority>(22);
         service->sendToPhone(p);
     } else {
         LOG_INFO("Send packet to mesh");
