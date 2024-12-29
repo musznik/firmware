abe39fb06ee5d919d2817f6d976d61e2804ac59e
musznik <bymucha@gmail.com>
2024-12-29 03:18:19 +0100

    sniffer memory fix?

--- src/mesh/Router.cpp
+++ src/mesh/Router.cpp
@@ -311,9 +311,39 @@ bool Router::cancelSending(NodeNum from, PacketId id)
  */
 void Router::sniffReceived(const meshtastic_MeshPacket *p, const meshtastic_Routing *c)
 {
-    if(p->from != 0){
-        service->sendToPhoneRaw(packetPool.allocCopy(*p));
+    service->sendToPhoneRaw(packetPool.allocCopy(*p));
+}
+
+void MeshService::sendToPhoneRaw(meshtastic_MeshPacket *p)
+{
+#ifdef ARCH_ESP32
+#if !MESHTASTIC_EXCLUDE_STOREFORWARD
+    if (moduleConfig.store_forward.enabled && storeForwardModule->isServer() &&
+        p->decoded.portnum == meshtastic_PortNum_TEXT_MESSAGE_APP) {
+        releaseToPool(p); // Copy is already stored in StoreForward history
+        fromNum++;        // Notify observers for packet from radio
+        return;
     }
+#endif
+#endif
+
+    if (toPhoneQueue.numFree() == 0) {
+        if (p->decoded.portnum == meshtastic_PortNum_TEXT_MESSAGE_APP ||
+            p->decoded.portnum == meshtastic_PortNum_RANGE_TEST_APP) {
+            LOG_WARN("Sniff, ToPhone queue is full, discard oldest");
+            meshtastic_MeshPacket *d = toPhoneQueue.dequeuePtr(0);
+            if (d)
+                releaseToPool(d);
+        } else {
+            LOG_WARN("Sniff, ToPhone queue is full, drop packet");
+            releaseToPool(p);
+            fromNum++; // Make sure to notify observers in case they are reconnected so they can get the packets
+            return;
+        }
+    }
+
+    assert(toPhoneQueue.enqueue(p, 0));
+    fromNum++;
 }
 
 bool perhapsDecode(meshtastic_MeshPacket *p)
