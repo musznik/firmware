1d9113263e9c8f8f3f3c2c393b11981c31c61e71
musznik <bymucha@gmail.com>
2025-01-03 15:08:02 +0100

    airlimit fixing

--- src/airtime.h
+++ src/airtime.h
@@ -67,9 +67,9 @@ class AirTime : private concurrency::OSThread
     uint8_t lastUtilPeriod = 0;
     uint8_t lastUtilPeriodTX = 0;
     uint32_t secSinceBoot = 0;
-    uint8_t max_channel_util_percent = 80;
-    uint8_t polite_channel_util_percent = 80;
-    uint8_t polite_duty_cycle_percent = 80; // half of Duty Cycle allowance is ok for metadata
+    uint8_t max_channel_util_percent = 50;
+    uint8_t polite_channel_util_percent = 40;
+    uint8_t polite_duty_cycle_percent = 60; // half of Duty Cycle allowance is ok for metadata
 
     struct airtimeStruct {
         uint32_t periodTX[PERIODS_TO_LOG];     // AirTime transmitted
