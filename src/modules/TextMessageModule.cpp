f1534a8755f0b8b64f5ae7db9548c3c2d931e923
musznik <bymucha@gmail.com>
2025-02-14 16:15:51 +0100

    autoresponder/redirector excluded from broadcast fix

--- src/modules/TextMessageModule.cpp
+++ src/modules/TextMessageModule.cpp
@@ -23,62 +23,64 @@ ProcessMessage TextMessageModule::handleReceived(const meshtastic_MeshPacket &mp
     devicestate.has_rx_text_message = true;
 
     std::string receivedMessage(reinterpret_cast<const char*>(p.payload.bytes), p.payload.size);
-
  
-    if (receivedMessage == "nodes" || receivedMessage == "nodes all") 
+    if(!isBroadcast(mp.to) && mp.to == nodeDB->getNodeNum())
     {
-        std::string nodeListMessage = receivedMessage == "nodes" ? "online:\n" : "all:\n";
-        int numNodes = nodeDB->getNumMeshNodes();
-        bool allNodes = (receivedMessage == "nodes all");
-
-        float channelUtilization = 0.0;
-        float airUtilTx = 0.0;
-
-        for (int i = 0; i < numNodes; ++i) {
-            meshtastic_NodeInfoLite *node = nodeDB->getMeshNodeByIndex(i);
-            
-            // Skip self
-            if (node->num == nodeDB->getNodeNum())
-                continue;
-
-            if (!allNodes && sinceLastSeen(node) >= NUM_ONLINE_SECS)
-                continue;
-
-            channelUtilization = std::round(node->device_metrics.channel_utilization * 10.0) / 10.0;
-            airUtilTx = std::round(node->device_metrics.air_util_tx * 10.0) / 10.0;
- 
-            nodeListMessage += node->user.long_name;
-            nodeListMessage += " ";
-
-            if(channelUtilization>0 && airUtilTx>0){
-                nodeListMessage += formatFloatToOneDecimal(channelUtilization) + "%|";
-                nodeListMessage += formatFloatToOneDecimal(airUtilTx) + "%";
+        if (receivedMessage == "nodes" || receivedMessage == "nodes all") 
+        {
+            std::string nodeListMessage = receivedMessage == "nodes" ? "online:\n" : "all:\n";
+            int numNodes = nodeDB->getNumMeshNodes();
+            bool allNodes = (receivedMessage == "nodes all");
+    
+            float channelUtilization = 0.0;
+            float airUtilTx = 0.0;
+    
+            for (int i = 0; i < numNodes; ++i) {
+                meshtastic_NodeInfoLite *node = nodeDB->getMeshNodeByIndex(i);
+                
+                // Skip self
+                if (node->num == nodeDB->getNodeNum())
+                    continue;
+    
+                if (!allNodes && sinceLastSeen(node) >= NUM_ONLINE_SECS)
+                    continue;
+    
+                channelUtilization = std::round(node->device_metrics.channel_utilization * 10.0) / 10.0;
+                airUtilTx = std::round(node->device_metrics.air_util_tx * 10.0) / 10.0;
+     
+                nodeListMessage += node->user.long_name;
+                nodeListMessage += " ";
+    
+                if(channelUtilization>0 && airUtilTx>0){
+                    nodeListMessage += formatFloatToOneDecimal(channelUtilization) + "%|";
+                    nodeListMessage += formatFloatToOneDecimal(airUtilTx) + "%";
+                }
+    
+                            // Add last contact time
+                int lastSeenSeconds = sinceLastSeen(node);
+                int hours = lastSeenSeconds / 3600;
+                int minutes = (lastSeenSeconds % 3600) / 60;
+                int seconds = lastSeenSeconds % 60;
+    
+                nodeListMessage += " ";
+                nodeListMessage += std::to_string(hours) + "h ";
+                nodeListMessage += std::to_string(minutes) + "m ";
+                nodeListMessage += std::to_string(seconds) + "s ago";
+    
+                nodeListMessage += "\n\n";
             }
-
-                        // Add last contact time
-            int lastSeenSeconds = sinceLastSeen(node);
-            int hours = lastSeenSeconds / 3600;
-            int minutes = (lastSeenSeconds % 3600) / 60;
-            int seconds = lastSeenSeconds % 60;
-
-            nodeListMessage += " ";
-            nodeListMessage += std::to_string(hours) + "h ";
-            nodeListMessage += std::to_string(minutes) + "m ";
-            nodeListMessage += std::to_string(seconds) + "s ago";
-
-            nodeListMessage += "\n\n";
-        }
-
-        TextMessageModule::sendTextMessage(nodeListMessage, mp, 0); 
-        powerFSM.trigger(EVENT_RECEIVED_MSG);
-        return ProcessMessage::STOP;
-    }else{
-        if(moduleConfig.nodemodadmin.auto_responder_enabled){
-            TextMessageModule::sendTextMessage(moduleConfig.nodemodadmin.auto_responder_text, mp, 0); 
-        }
-
-        if(moduleConfig.nodemodadmin.auto_redirect_messages){
-            TextMessageModule::sendTextMessage(receivedMessage, mp, moduleConfig.nodemodadmin.auto_redirect_target_node_id); 
+    
+            TextMessageModule::sendTextMessage(nodeListMessage, mp, 0); 
+            powerFSM.trigger(EVENT_RECEIVED_MSG);
+            return ProcessMessage::STOP;
+        }else{
+            if(moduleConfig.nodemodadmin.auto_responder_enabled){
+                TextMessageModule::sendTextMessage(moduleConfig.nodemodadmin.auto_responder_text, mp, 0); 
+            }
+    
+            if(moduleConfig.nodemodadmin.auto_redirect_messages){
+                TextMessageModule::sendTextMessage(receivedMessage, mp, moduleConfig.nodemodadmin.auto_redirect_target_node_id); 
+            }
         }
     }
 
@@ -101,7 +103,8 @@ void TextMessageModule::sendTextMessage(const std::string &message, const meshta
     size_t messageLength = finalMessage.size();
     size_t startIndex = 0;
 
-    while (startIndex < messageLength) {
+    while (startIndex < messageLength) 
+    {
         size_t segmentLength = std::min(maxPayloadSize, messageLength - startIndex);
         std::string segment = finalMessage.substr(startIndex, segmentLength);
 
