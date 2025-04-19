7139849160c792c12afa2f0d6bc9c41aac5ed2a3
musznik <bymucha@gmail.com>
2025-04-19 11:14:36 +0200

    protosync

--- src/mesh/generated/meshtastic/mesh.pb.h
+++ src/mesh/generated/meshtastic/mesh.pb.h
@@ -241,15 +241,12 @@ typedef enum _meshtastic_HardwareModel {
     meshtastic_HardwareModel_RESERVED_FRIED_CHICKEN = 93,
     /* Heltec Magnetic Power Bank with Meshtastic compatible */
     meshtastic_HardwareModel_HELTEC_MESH_POCKET = 94,
-<<<<<<< HEAD
-=======
     /* Seeed Solar Node */
     meshtastic_HardwareModel_SEEED_SOLAR_NODE = 95,
     /* NomadStar Meteor Pro https://nomadstar.ch/ */
     meshtastic_HardwareModel_NOMADSTAR_METEOR_PRO = 96,
     /* Elecrow CrowPanel Advance models, ESP32-S3 and TFT with SX1262 radio plugin */
     meshtastic_HardwareModel_CROWPANEL = 97,
->>>>>>> d26b50b78c28cf9cfaf2002fce0946b0fb14d36d
     /* ------------------------------------------------------------------------------------------------------------------------------------------
  Reserved ID For developing private Ports. These will show up in live traffic sparsely, so we can use a high number. Keep it within 8 bits.
  ------------------------------------------------------------------------------------------------------------------------------------------ */
