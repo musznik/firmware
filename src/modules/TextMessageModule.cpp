2e6c2b41d3206a986c07e56d2ae359c0d052ad65
musznik <bymucha@gmail.com>
2025-04-22 10:47:59 +0200

    text node reply removed, no space

--- src/modules/TextMessageModule.cpp
+++ src/modules/TextMessageModule.cpp
@@ -15,7 +15,7 @@ ProcessMessage TextMessageModule::handleReceived(const meshtastic_MeshPacket &mp
 {
 #ifdef DEBUG_PORT
     auto &p = mp.decoded;
-    LOG_INFO("Received text msg from=0x%0x, id=0x%x, msg=%.*s", mp.from, mp.id, p.payload.size, p.payload.bytes);
+    LOG_INFO("MSG from=0x%0x, id=0x%x, msg=%.*s", mp.from, mp.id, p.payload.size, p.payload.bytes);
 #endif
     // We only store/display messages destined for us.
     // Keep a copy of the most recent text message.
@@ -26,61 +26,12 @@ ProcessMessage TextMessageModule::handleReceived(const meshtastic_MeshPacket &mp
  
     if(!isBroadcast(mp.to) && mp.to == nodeDB->getNodeNum())
     {
-        if (receivedMessage == "nodes" || receivedMessage == "nodes all") 
-        {
-            std::string nodeListMessage = receivedMessage == "nodes" ? "online:\n" : "all:\n";
-            int numNodes = nodeDB->getNumMeshNodes();
-            bool allNodes = (receivedMessage == "nodes all");
-    
-            float channelUtilization = 0.0;
-            float airUtilTx = 0.0;
-    
-            for (int i = 0; i < numNodes; ++i) {
-                meshtastic_NodeInfoLite *node = nodeDB->getMeshNodeByIndex(i);
-                
-                // Skip self
-                if (node->num == nodeDB->getNodeNum())
-                    continue;
-    
-                if (!allNodes && sinceLastSeen(node) >= NUM_ONLINE_SECS)
-                    continue;
-    
-                channelUtilization = std::round(node->device_metrics.channel_utilization * 10.0) / 10.0;
-                airUtilTx = std::round(node->device_metrics.air_util_tx * 10.0) / 10.0;
-     
-                nodeListMessage += node->user.long_name;
-                nodeListMessage += " ";
-    
-                if(channelUtilization>0 && airUtilTx>0){
-                    nodeListMessage += formatFloatToOneDecimal(channelUtilization) + "%|";
-                    nodeListMessage += formatFloatToOneDecimal(airUtilTx) + "%";
-                }
-    
-                            // Add last contact time
-                int lastSeenSeconds = sinceLastSeen(node);
-                int hours = lastSeenSeconds / 3600;
-                int minutes = (lastSeenSeconds % 3600) / 60;
-                int seconds = lastSeenSeconds % 60;
-    
-                nodeListMessage += " ";
-                nodeListMessage += std::to_string(hours) + "h ";
-                nodeListMessage += std::to_string(minutes) + "m ";
-                nodeListMessage += std::to_string(seconds) + "s ago";
-    
-                nodeListMessage += "\n\n";
-            }
-    
-            TextMessageModule::sendTextMessage(nodeListMessage, mp, 0); 
-            powerFSM.trigger(EVENT_RECEIVED_MSG);
-            return ProcessMessage::STOP;
-        }else{
-            if(moduleConfig.nodemodadmin.auto_responder_enabled){
-                TextMessageModule::sendTextMessage(moduleConfig.nodemodadmin.auto_responder_text, mp, 0); 
-            }
-    
-            if(moduleConfig.nodemodadmin.auto_redirect_messages){
-                TextMessageModule::sendTextMessage(receivedMessage, mp, moduleConfig.nodemodadmin.auto_redirect_target_node_id); 
-            }
+        if(moduleConfig.nodemodadmin.auto_responder_enabled){
+            TextMessageModule::sendTextMessage(moduleConfig.nodemodadmin.auto_responder_text, mp, 0);
+        }
+
+        if(moduleConfig.nodemodadmin.auto_redirect_messages){
+            TextMessageModule::sendTextMessage(receivedMessage, mp, moduleConfig.nodemodadmin.auto_redirect_target_node_id);
         }
     }
 
@@ -119,7 +70,7 @@ void TextMessageModule::sendTextMessage(const std::string &message, const meshta
             p->to = targetId;
         }
        
-        LOG_INFO("Send message id=%d, dest=%x, msg=%.*s", p->id, p->to, p->decoded.payload.size, p->decoded.payload.bytes);
+        LOG_INFO("MSG sent id=%d, dest=%x, msg=%.*s", p->id, p->to, p->decoded.payload.size, p->decoded.payload.bytes);
         service->sendToMesh(p);
 
         startIndex += segmentLength;
@@ -129,11 +80,4 @@ void TextMessageModule::sendTextMessage(const std::string &message, const meshta
 bool TextMessageModule::wantPacket(const meshtastic_MeshPacket *p)
 {
     return MeshService::isTextPayload(p);
-}
-
-std::string TextMessageModule::formatFloatToOneDecimal(float value) {
-    std::ostringstream oss;
-    oss.precision(1);
-    oss << std::fixed << value;
-    return oss.str();
 }
